eba577890f89c0cefbb8e63ab2990c0d18aad1a9
Kortex <jfcontart@idemobi.com>
2026-06-14 10:46:11 +0200

    [UPGRADE]

---
+++ UnitTests/TextAreaFieldBuilderTests.cs
@@ -0,0 +1,103 @@
+#region Copyright
+
+// ©2002-2026 idéMobi
+// www.idemobi.com
+
+#endregion
+
+#region
+
+using DMBFormBuilder;
+using DMBFormBuilder.Resources;
+using DMBServerHelper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using NUnit.Framework;
+
+#endregion
+
+namespace DMBFormBuilderUnitTest;
+
+[TestFixture]
+public sealed class TextAreaFieldBuilderTests
+{
+    [Test]
+    public void SetNoHtmlUsesTranslatedBadgeText()
+    {
+        ICombinedStringLocalizer initialLocalizer = WebLocalizer.DataAnnotationLocalizer;
+        CombinedStringLocalizer testLocalizer = new();
+        testLocalizer.InjectResource("FormBuilderTextArea", new DictionaryStringLocalizer(new Dictionary<string, string>
+        {
+            [nameof(DMBFormBuilderDataAnnotationLocalization.FormBuilder_Field_NoHtmlInvalid)] = "No HTML not accepted.",
+            [nameof(DMBFormBuilderDataAnnotationLocalization.FormBuilder_Field_Badge_NoHtml)] = "No HTML"
+        }));
+
+        WebLocalizer.DataAnnotationLocalizer = testLocalizer;
+
+        try
+        {
+            IHtmlHelper html = TestHtmlHelperFactory.Create();
+            TextAreaFieldBuilder builder = new TextAreaFieldBuilder(new StringWriter(), html)
+                .SetInput("Message", "Message")
+                .SetNoHtml();
+
+            string markup = builder.ToHtmlString();
+
+            Assert.That(markup, Does.Contain(">No HTML<"));
+        }
+        finally
+        {
+            WebLocalizer.DataAnnotationLocalizer = initialLocalizer;
+        }
+    }
+
+    private sealed class DictionaryStringLocalizer : IStringLocalizer
+    {
+        #region Instance fields and properties
+
+        private readonly Dictionary<string, string> _values;
+
+        #endregion
+
+        #region Instance constructors and destructors
+
+        public DictionaryStringLocalizer(Dictionary<string, string> values)
+        {
+            _values = values;
+        }
+
+        #endregion
+
+        #region Instance methods
+
+        #region From interface IStringLocalizer
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            return _values.Select(item => new LocalizedString(item.Key, item.Value, resourceNotFound: false));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Instance indexers
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                if (_values.TryGetValue(name, out string? value))
+                {
+                    return new LocalizedString(name, value, resourceNotFound: false);
+                }
+
+                return new LocalizedString(name, name, resourceNotFound: true);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments] => this[name];
+
+        #endregion
+    }
+}
